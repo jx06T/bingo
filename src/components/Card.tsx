50d7b6fe9befa6da14647fbc2cde75a6a4ed2775
jx06T <50313tjx06@gmail.com>
2024-10-31 12:12:59 +0800

    亂改css

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -51,18 +51,18 @@ function Card({ state, imgPath, describe, name }: ExtendedCardFormat) {
             </div>
 
             {/* <div className={` describe ${state.show ? " show fixed left-0 right-0 top-0 bottom-0 pointer-events-auto " : "fixed left-[50%] right-[50%] top-[50%] bottom-[50%] pointer-events-none "} transition-all duration-500 flex justify-center items-center z-20 glass-2`}> */}
-            <div className={` describe ${state.show ? " show " : ""} transition-all duration-500  z-20 glass-2`}>
-                <div className={` bgi relative bg-slate-300 pointer-events-auto overflow-y-hidden `}>
-                    <div className="  rounded-md card-img">
+            <div className={` describe ${state.show ? " show " : ""} transition-all duration-500  z-20 glass-2 overflow-y-hidden`}>
+                <div className={` bgi relative bg-slate-300 pointer-events-auto md:overflow-y-hidden overflow-y-auto`}>
+                    <div className="  rounded-md card-img  block">
                         <img
                             src={imgPath}
                             alt={name}
-                            className=" w-full rounded-md h-full object-cover"
+                            className=" w-full rounded-md h-full object-cover block"
                         />
                     </div>
-                    <div className={`${showText ? " opacity-100" : " opacity-0"} overflow-y-auto transition-all duration-300 h-full w-full py-1 flex flex-col justify-around items-center`}>
-                        <div className="space-y-3 text-sm md:text-2xl lg:text-3xl mt-20 ">
-                            <h1 className="w-full mb-1 text-center md:text-4xl lg:text-5xl font-medium -mt-16 ">{name}</h1>
+                    <div className={`${showText ? " opacity-100" : " opacity-0"} md:overflow-y-auto transition-all duration-300 h-full w-full py-1 flex flex-col justify-start items-center `}>
+                        <h1 className="w-full mb-1 text-center text-3xl md:text-4xl lg:text-5xl font-medium ">{name}</h1>
+                        <div className="space-y-3 text-lg md:text-2xl lg:text-3xl ">
                             <hr />
                             <pre className="w-full text-wrap break-words whitespace-pre-line">{describe.ch}</pre>
                             <hr />
@@ -70,7 +70,8 @@ function Card({ state, imgPath, describe, name }: ExtendedCardFormat) {
                             <hr />
                             <pre className="w-full text-wrap break-words whitespace-pre-line">{describe.jp}</pre>
                         </div>
-                        <div className=" mt-3 pb-0">
+
+                        <div className=" my-3 p-2 w-24 h-24 md:w-40 md:h-40 lg:w-52 lg:h-52">
                             <div className="mb-8 p-5 bg-slate-400 w-full h-full aspect-square-j rounded-full big-text text-center font-extrabold border-0 border-slate-200 flex justify-center items-center">
                                 <span>
                                     {state.number}
@@ -82,7 +83,7 @@ function Card({ state, imgPath, describe, name }: ExtendedCardFormat) {
                         onClick={handleClickOP}
                         className="right-1 top-2 absolute text-2xl transition-transform duration-300 hover:scale-110"
                     >
-                        <MaterialSymbolsCancelOutlineRounded className="text-3xl text-white" />
+                        <MaterialSymbolsCancelOutlineRounded className="text-3xl md:text-4xl text-white" />
                     </button>
                 </div>
             </div>
