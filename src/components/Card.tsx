1d62db554f67a1f56a40fb985ebdc5a0c9513aeb
jx06T <50313tjx06@gmail.com>
2024-10-31 12:20:48 +0800

    亂改css

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -52,7 +52,7 @@ function Card({ state, imgPath, describe, name }: ExtendedCardFormat) {
 
             {/* <div className={` describe ${state.show ? " show fixed left-0 right-0 top-0 bottom-0 pointer-events-auto " : "fixed left-[50%] right-[50%] top-[50%] bottom-[50%] pointer-events-none "} transition-all duration-500 flex justify-center items-center z-20 glass-2`}> */}
             <div className={` describe ${state.show ? " show " : ""} transition-all duration-500  z-20 glass-2 overflow-y-hidden`}>
-                <div className={` bgi relative bg-slate-300 pointer-events-auto md:overflow-y-hidden overflow-y-auto`}>
+                <div className={` bgi relative bg-slate-300 pointer-events-auto sm:overflow-y-hidden overflow-y-auto`}>
                     <div className="  rounded-md card-img  block">
                         <img
                             src={imgPath}
@@ -60,7 +60,7 @@ function Card({ state, imgPath, describe, name }: ExtendedCardFormat) {
                             className=" w-full rounded-md h-full object-cover block"
                         />
                     </div>
-                    <div className={`${showText ? " opacity-100" : " opacity-0"} md:overflow-y-auto transition-all duration-300 h-full w-full py-1 flex flex-col justify-start items-center `}>
+                    <div className={`${showText ? " opacity-100" : " opacity-0"} sm:overflow-y-auto transition-all duration-300 h-full w-full py-1 flex flex-col justify-start items-center `}>
                         <h1 className="w-full mb-1 text-center text-3xl md:text-4xl lg:text-5xl font-medium ">{name}</h1>
                         <div className="space-y-3 text-lg md:text-2xl lg:text-3xl ">
                             <hr />
